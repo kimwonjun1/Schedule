849b5530412ab413b82e7bb929a53292f9afb743
kimwonjun1 <monkeydmw@naver.com>
2025-05-12 13:56:28 +0900

    .gitignore modified

---
+++ src/test/java/com/capstone/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
