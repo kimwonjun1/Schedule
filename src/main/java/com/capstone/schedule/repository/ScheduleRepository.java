77bf4e5aef713c97e3d0514fa22e5bc124f3de51
kimwonjun1 <monkeydmw@naver.com>
2025-05-14 01:35:56 +0900

    ScheduleRepository write

--- src/main/java/com/capstone/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/capstone/schedule/repository/ScheduleRepository.java
@@ -1,4 +1,20 @@
 package com.capstone.schedule.repository;
 
+import com.capstone.schedule.dto.ScheduleResponseDto;
+import com.capstone.schedule.entity.Schedule;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ScheduleRepository {
+    ScheduleResponseDto saveSchedule(Schedule schedule);
+
+    List<Schedule> findAllSchedules(String modificationDate, String name, Long writerId);
+
+    Schedule findScheduleByIdOrElseThrow(Long id);
+
+    int updateWork(Long id, String work, String password);
+
+    int deleteSchedule(Long id);
+
 }
