965d9dc6f48fc8a2e44cc05575b911c83760d948
kimwonjun1 <monkeydmw@naver.com>
2025-05-14 01:34:18 +0900

    ScheduleRequestDto write

--- src/main/java/com/capstone/schedule/dto/ScheduleRequestDto.java
+++ src/main/java/com/capstone/schedule/dto/ScheduleRequestDto.java
@@ -1,2 +1,12 @@
-package com.capstone.schedule.dto;public class ScheduleRequestDto {
+package com.capstone.schedule.dto;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ScheduleRequestDto {
+    private String work;
+    private Long writerId;
+    private String password;
 }
