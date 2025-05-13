1679faff2d26910b6775b0d25acaf7bc1e764df5
kimwonjun1 <monkeydmw@naver.com>
2025-05-14 01:37:06 +0900

    ScheduleService write

--- src/main/java/com/capstone/schedule/service/ScheduleService.java
+++ src/main/java/com/capstone/schedule/service/ScheduleService.java
@@ -1,2 +1,14 @@
-package com.capstone.schedule.service;public interface ScheduleService {
+package com.capstone.schedule.service;
+
+import com.capstone.schedule.dto.ScheduleRequestDto;
+import com.capstone.schedule.dto.ScheduleResponseDto;
+
+import java.util.List;
+
+public interface ScheduleService {
+    ScheduleResponseDto saveSchedule(ScheduleRequestDto dto);
+    List<ScheduleResponseDto> findAllSchedules(String modificationDate, String name, Long writerId);
+    ScheduleResponseDto findScheduleById(Long id);
+    ScheduleResponseDto updateWork(Long id, String work, String password);
+    void deleteSchedule(Long id);
 }
