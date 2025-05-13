583982caab7bcf627cd576a095f26d6cf1dccbc0
kimwonjun1 <monkeydmw@naver.com>
2025-05-14 01:35:02 +0900

    ScheduleResponseDto write

--- src/main/java/com/capstone/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/capstone/schedule/dto/ScheduleResponseDto.java
@@ -1,2 +1,27 @@
-package com.capstone.schedule.dto;public class ScheduleResponseDto {
+package com.capstone.schedule.dto;
+
+import com.capstone.schedule.entity.Schedule;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ScheduleResponseDto {
+    private Long id;
+    private String work;
+    private String password;
+    private LocalDateTime creationDate;
+    private LocalDateTime modificationDate;
+    private Long writerId;
+
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id = schedule.getId();
+        this.work = schedule.getWork();
+        this.writerId = schedule.getWriterId();
+        this.password = schedule.getPassword();
+        this.creationDate = schedule.getCreationDate();
+        this.modificationDate = schedule.getModificationDate();
+    }
 }
