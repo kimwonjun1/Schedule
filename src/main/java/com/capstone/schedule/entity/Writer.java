8602e07b076a227f688fc4e3c047af68fd8d2ee2
kimwonjun1 <monkeydmw@naver.com>
2025-05-14 01:33:36 +0900

    Writer entity write

--- src/main/java/com/capstone/schedule/entity/Writer.java
+++ src/main/java/com/capstone/schedule/entity/Writer.java
@@ -1,2 +1,17 @@
-package com.capstone.schedule.entity;public class Writer {
+package com.capstone.schedule.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class Writer {
+    private Long id;                            // 작성자 식별자
+    private String name;                        // 작성자명
+    private String email;                       // 이메일
+    private LocalDateTime creationDate;         // 작성일
+    private LocalDateTime modificationDate;     // 수정일
 }
